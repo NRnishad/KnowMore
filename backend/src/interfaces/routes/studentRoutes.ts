5546bf51a60abae830cffbf8a358085de70f3e02
NRnishad <nrnishad106@gmail.com>
2025-10-20 17:09:40 +0530

    student home and profile updated

---
+++ backend/src/interfaces/routes/studentRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import { isAuthenticated, authorizeRole } from "../middlewares/authMiddleware";
+import { studentHome, updateProfileImageController } from "../controllers/student/profile/profileController";
+import { upload } from "../../infrastructure/middlewares/multer";
+
+const router = Router();
+
+router.get('/home', isAuthenticated, authorizeRole(['student','instructor']), studentHome)
+router.post('/profile/:id/update-image', upload.single('profileImage'), isAuthenticated, authorizeRole(['student','instructor']), updateProfileImageController)
+
+
+export default router;
